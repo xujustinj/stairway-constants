896dbf1b449743b70fa128fba1121c0b23430489
Justin Xu <xu.justin.j@gmail.com>
2026-03-04 01:48:23 -0500

    WIP

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,5 +1,6 @@
 <script id="setup-tex">
   import { onMount } from "svelte";
+  import Falling from "./Falling.svelte";
 
   // We could also do this directly in index.html, but it doesn't work on page refresh.
   // Thanks to https://svelte.dev/repl/ebd12dbf04574b2bb40003cc8a4299b0
@@ -22,24 +23,65 @@
 </script>
 
 <main>
-  <h1>Stairway Constants</h1>
-  <img
-    src="./images/stairway-constants-icon-old-290x290.png"
-    alt="symbols for all the stairway constants"
-    id="constant-symbols"
-  />
-  <p><b>UNDER CONSTRUCTION</b></p>
-  <p>
-    Here will eventually be the web revision of <i>Stairway Constants</i>: all 8
-    floors of constants rolled up into one scrolling webpage.
-  </p>
-  <p>Until then, enjoy some \(\LaTeX\) filler! \[\Huge \pi^\pi\]</p>
+  <div id="content">
+    <div id="start">
+      <div id="start-content">
+        <h1 id="title">STAIRWAY<br>CONSTANTS</h1>
+        <p>scroll up to begin</p>
+      </div>
+      <div id="start-background" aria-hidden="true">
+        <Falling distance=5 phase=0.1 position=0.5>\(\pi\)</Falling>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style lang="css">
-  #constant-symbols {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
+  #content {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+
+  #start {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+
+  #start-content {
+    top: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #start-background {
+    top: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    z-index: -1;
+    color: #C60078;
+  }
+
+  h1 {
+    font-size: min(10.0vw, 20.0vh);
+    font-weight: bolder;
+  }
+
+  p {
+    font-size: min(2vw, 4vh);
+    font-weight: normal;
+  }
+
+  #title {
+    text-align: center;
+    line-height: 1;
+    width: fit-content;
   }
 </style>
---
+++ app/src/Falling.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let position: number;
+  export let distance: number;
+  export let phase: number;
+  const duration = distance;
+  const size = 10.0 / distance;
+  const opacity = Math.exp(Math.min(0.0, -distance / 20.0));
+  const delay = duration * phase;
+</script>
+
+<span class="falling" style="
+  visibility: hidden;
+  position: absolute;
+  left: {100*position}%;
+  visibility: hidden;
+  animation-duration: {duration}s;
+  animation-delay: {delay}s;
+  font-size: min({size}vw, {size * 2}vh);
+  width: 25%;
+  text-align: center;
+  opacity: {opacity};
+">
+  <slot></slot>
+</span>
+
+<style lang="css">
+  @keyframes fall {
+    from {
+      transform: translate(-50%, -100%);
+      visibility: visible;
+    }
+    to {
+      transform: translate(-50%, 100vh);
+      visibility: visible;
+    }
+  }
+
+  .falling {
+    animation-name: fall;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+</style>
